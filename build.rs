f284d6a28cf082f037ebd60b5455443012bc5ada
Xerxes-2 <dspxue@gmail.com>
2024-11-15 21:52:59 +1100

    refactor: streamline prost_build configuration in build.rs

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    let mut config = prost_build::Config::new();
-    config.out_dir("src/proto");
-    config.type_attribute(
-        "lq.ViewSlot",
-        "#[derive(::serde::Serialize, ::serde::Deserialize)]",
-    );
-    config.compile_protos(&["proto/liqi.proto"], &["proto/"])?;
+    prost_build::Config::new()
+        .out_dir("src/proto")
+        .type_attribute(
+            "lq.ViewSlot",
+            "#[derive(::serde::Serialize, ::serde::Deserialize)]",
+        )
+        .compile_protos(&["proto/liqi.proto"], &["proto/"])?;
     // cargo fmt
     std::process::Command::new("cargo").arg("fmt").status()?;
     Ok(())
