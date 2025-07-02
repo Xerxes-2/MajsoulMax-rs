9a2b27fb34ee6f18ac5b0b490c8fd1ae11290cc7
Xerxes-2 <dspxue@gmail.com>
2025-07-02 11:07:18 +1000

    Refactor code structure for improved readability and maintainability

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ use std::{env, io::Result, path::PathBuf};
 
 fn main() -> Result<()> {
     prost_build::Config::new()
-        .out_dir("src/proto")
         .type_attribute(
             "lq.ViewSlot",
             "#[derive(::serde::Serialize, ::serde::Deserialize)]",
@@ -11,7 +10,8 @@ fn main() -> Result<()> {
             PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
                 .join("liqi_desc.bin"),
         )
-        .compile_protos(&["proto/liqi.proto"], &["proto/"])?;
+        .out_dir("src/proto")
+        .compile_protos(&["proto/liqi.proto", "proto/sheets.proto"], &["proto/"])?;
 
     Ok(())
 }
